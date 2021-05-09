7d4d4c8f6c0b28e802ea58cc7587e6f0f9a9a342
Greg <gdrew21@gmail.com>
2021-05-09 12:04:42 -0400

    Adding functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { nanoid } from "nanoid";
 import FilterButton from './components/FilterButton';
 import Form from './components/Form';
 import Todo from './components/Todo';
 
 function App({ tasks }) {
-  const taskList = tasks.map(task => 
-    <Todo name={task.name} completed={task.completed} key={task.id} id={task.id} />);
+
+  const [localTaskList, setLocalTaskList] = useState(tasks);
+
+  const toggleTaskCompleted = (id) => {
+    const updatedTask = localTaskList.map(task => {
+      if (id === task.id) {
+        return {...task, completed: !task.completed}
+      }
+      return task;
+    })
+    console.log(updatedTask)
+    setLocalTaskList(updatedTask)
+  }
+
+  const taskList = localTaskList.map(task => 
+    <Todo 
+      name={task.name} 
+      completed={task.completed} 
+      key={task.id} 
+      id={task.id} 
+      toggleTaskCompleted={toggleTaskCompleted}
+      />
+    );
+
+  const handleTask = (name) => {
+    const newTask = {id: "todo-" + nanoid(), name: name, completed: false };
+    setLocalTaskList([...localTaskList, newTask])
+  }
+
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
 
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Form />
+      <Form addNameFunc={handleTask} />
       <div className="filters btn-group stack-exception">
         <FilterButton category="All" />
-        <FilterButton />
-        <FilterButton />
+        <FilterButton category="Completed" />
+        <FilterButton category="Remaining" />
       </div>
       <h2 id="list-heading">
-        3 tasks remaining
+        {headingText}
       </h2>
       <ul
         role="list"
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Form = () => {
+const Form = ({ addNameFunc }) => {
+    const [name, setName] = useState('')
+
+    const handleChange = (e) => {
+        setName(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        console.log('handle submit clicked')
+        e.preventDefault();
+        if(name.length > 0) {
+        addNameFunc(name);
+            }
+        setName("");
+    }
+
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h2 className="label-wrapper">
                     <label htmlFor="new-todo-input" className="label_lg">
                         What needs to be done?
@@ -15,6 +30,8 @@ const Form = () => {
                     className="input input_lg"
                     name="text"
                     autoComplete="off"
+                    value={name}
+                    onChange={handleChange}
                 />
                 <button type="submit" className="btn btn_primary btn_lg">
                     Add
